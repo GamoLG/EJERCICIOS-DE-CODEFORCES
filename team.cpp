d8985bc7d9d2951bf26bdaf3f4840406f7c459d1
gamoLG <224871@unsaac.edu.pe>
2026-02-20 09:59:44 -0500

    ejercicio 3: team

---
+++ team.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n; cin>>n;
+	int a,b,c;	
+	int r=0;
+	for(int i=0;i<n;i++)
+	{
+		cin>>a; cin>>b; cin>>c;
+		//tambien se puede hacer con sumas de valores que si es 
+		//mayor o igual a 2
+		//if((a+b+c)>=2)
+	
+		if((a==1 and b==1) or (a==1 and c==1) or (b==1 and c==1))
+		{
+			r++;
+		}
+	}
+	cout<<r;
+	
+	
+	
+	return 0;
+}
